2deeea371ec2086d57e46932123bce383621b5be
Mohammadreza Aboutalebi <33571724+Parham75@users.noreply.github.com>
2018-04-28 19:10:36 +0100

    lol

--- Orbital Manoeuvres/StarterPane.java
+++ Orbital Manoeuvres/StarterPane.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 import javax.swing.event.*;
 public class StarterPane extends JPanel
 {
-    JLabel jlIntro = new JLabel("Welcome to the Maneuver Calculator");
-    JLabel jlChoose = new JLabel("Please Choose one of the Options");
-    
+    JLabel jlIntro = new JLabel("Welcome to the Satellite Chase");
+    JLabel jlIntro2 = new JLabel("Manoeuvres AI Simulator");
+    JLabel jlChoose = new JLabel("Please Choose one of");
+    JLabel jlChoose2 = new JLabel("the following Options");
     JButton jbHoh = new JButton("Circular Hohmann");
     JButton jbShowVelPos = new JButton ("Show Velocity and Position");
-    
     JButton jbAbout = new JButton("About Us");
     JButton jbExit = new JButton("Exit");
 
@@ -18,12 +18,12 @@ public class StarterPane extends JPanel
     public StarterPane(int space,int width,int height,Color LabelColor,Color BackColor)
     {
         
-        jlIntro.setBounds((int)(0.5*space),space,3*width,height);
-
-        jlChoose.setBounds(space,3*space+2*height,width,height);
+        jlIntro.setBounds((int)(1.25*space),space,3*width,height);
+        jlIntro2.setBounds((int)(2*space),2*space,3*width,height);
+        jlChoose.setBounds((int)(2.5*space),2*space+2*height,width,height);
+        jlChoose2.setBounds((int)(2.5*space),3*space+2*height,width,height);
         jbShowVelPos.setBounds(space,4*space+3*height,width,height);
         jbHoh.setBounds(space,5*space+4*height,width,height);
-        
         jbAbout.setBounds(space,7*space+6*height,width,height);
         // Exit Button should be half as wide as the other buttons
         int ExitWidth = width/2;
@@ -31,14 +31,16 @@ public class StarterPane extends JPanel
         
         this.setBackground(BackColor);
         jlIntro.setForeground(Color.white);
+        jlIntro2.setForeground(Color.white);
         jlChoose.setForeground(LabelColor);
+        jlChoose2.setForeground(LabelColor);
         
         add(jlIntro);
-
+        add(jlIntro2);
         add(jlChoose);
+        add(jlChoose2);
         add(jbShowVelPos);
         add(jbHoh);
-        
         add(jbAbout);
         add(jbExit);
     }// end of constructor of the class StarterPane
